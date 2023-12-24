9869f166e585f41ea03463c235953d07e0bc51b2
nguyenquangtruonghung <nguyenquangtruonghung99@gmail.com>
2023-12-25 05:34:42 +0700

    Create Project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
+import React from "react";
 import './App.css';
+import { BrowserRouter } from "react-router-dom";
+import MyComponent1 from "./components/LoginForm";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter basename="/my-project">
+      <div className="App">
+        <h1>DATN - Bui Minh Triet</h1>
+        <MyComponent1 />
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/showdata.js
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const Filldata = () => {
+  const [positionFilter, setPositionFilter] = useState('');
+  const [data, setData] = useState([]);
+  const [filteredData, setFilteredData] = useState([]);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    try {
+      const response = await axios.get('https://us-east-1.aws.data.mongodb-api.com/app/application-0-sznak/endpoint/finddata');
+      setData(response.data);
+    } catch (error) {
+      console.error('Lỗi khi lấy dữ liệu:', error);
+    }
+  };
+
+  const handleFilterChange = (event) => {
+    setPositionFilter(event.target.value);
+  };
+
+  const handleFilterSubmit = () => {
+    const filteredResults = data.filter(item => item.Public.Output.data.value.position === parseInt(positionFilter, 10));
+    setFilteredData(filteredResults);
+  };
+
+  return (
+    <div>
+      <label>
+        <h2>Nhập vị trí cần truy vấn :</h2>
+        <input type="text" value={positionFilter} onChange={handleFilterChange} />
+      </label>
+      <div>
+      <button onClick={handleFilterSubmit}>Submit</button>
+      </div>
+
+      <h3>Kết quả:</h3>
+      <ul>
+        {filteredData.map(item => (
+          <ni key={item._id}>
+            <p>Time: {item.Public.Output.data.Time}</p>
+            <p>DOA: {item.Public.Input.Data.DOA}</p>
+            <p>Pulse: {item.Public.Output.data.value.Pulse}</p>
+            <p>Position: {item.Public.Output.data.value.position}</p>
+            <p>Area: {item.Public.Output.data.value.area}</p>
+            <p>------------------------</p>
+          </ni>
+        ))}
+      </ul>
+    </div>
+  );
+};
+export default Filldata;
