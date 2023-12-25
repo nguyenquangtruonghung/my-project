dca9a4a608ef94e23033f587be394762f70bdc50
nguyenquangtruonghung <nguyenquangtruonghung99@gmail.com>
2023-12-25 07:12:24 +0700

    Update Project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import './App.css';
 import { BrowserRouter } from "react-router-dom";
-import MyComponent1 from "./Component/LoginForm";
+import MyComponent1 from "./components/LoginForm";
 
 function App() {
   return (
